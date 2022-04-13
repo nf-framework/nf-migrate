887d08d8143a82062c827d56dc001d38e0b56ffc
mascotze <andrmaxmail@gmail.com>
2022-04-13 16:31:08 +0300

    refactor gather.js and rename it to source-files due new functionality

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export * as utils from './lib/utils.js';
 export { NFMig, NFMigObj, NFMigDat } from './lib/migration.js';
-export * as gather from './lib/gather.js';
+export * as sourceFiles from './lib/source-files.js';
